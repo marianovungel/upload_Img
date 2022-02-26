3cb8728ac684f39f9c8c19d68ad6000aba795fb8
marianovungel <vungem88@gmail.com>
2022-02-25 22:31:47 -0300

    link do upload de imagens

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express=require('express')
 const app=express()
 const mongoose = require('mongoose')
 const morgan=require('morgan')
+const path = require("path")
 const RouterUpload = require('./routes.js')
 mongoose.connect(process.env.URL_MONGDB, {
     useNewUrlParser: true,
@@ -11,6 +12,7 @@ mongoose.connect(process.env.URL_MONGDB, {
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp", "uploads")))
 app.use("/", RouterUpload)
 
 const PORT=process.env.PORT
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const multer = require('multer')
 const multerConfig = require('./config/multer')
 const Post = require('./models/Post')
 
+
 router.post("/", multer(multerConfig).single('file'), async(req, res)=>{
     const { originalnama: name, size, key, location: url=""}= req.file;
     const post = await Post.create({
@@ -12,6 +13,15 @@ router.post("/", multer(multerConfig).single('file'), async(req, res)=>{
         url,
     })
     return res.json(post);
+})
+router.get("/", async(req, res)=>{
+    const post = await Post.find()
+    return res.json(post);
+})
+router.delete("/:id", async(req, res)=>{
+    const id = req.params.id;
+    await Post.findByIdAndDelete(id);
+    res.json({message: "imagem deletada com sucesso"})
 })
 
 
